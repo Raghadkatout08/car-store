6a0de31759a93a260e87b85c89ce956ef533a6eb
raghadkatout08 <raghadkatout67@gmail.com>
2024-07-22 19:31:08 +0000

    lab-25

---
+++ cars/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+# Create your models here.
+class Car(models.Model):
+    buyer_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    model = models.CharField(max_length=250)
+    brand = models.CharField(max_length=250)
+    price = models.IntegerField()
+    is_bought = models.BooleanField()
+    buy_time = models.DateField()
+
+    def __str__(self):
+        return self.brand
+    
+
+    def get_absolute_url(self):
+        return reverse("car_detail", args={self.id})
+    
